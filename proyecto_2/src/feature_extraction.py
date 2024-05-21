414f2f53484d8a4e902577bdaa2e1824443925f2
Dwayne <dwayne.taylor@ucr.ac.cr>
2024-05-21 16:48:15 -0600

    Made bilateral filtering preprocessing

--- proyecto_2/src/feature_extraction.py
+++ proyecto_2/src/feature_extraction.py
@@ -74,7 +74,6 @@ def _extract_lbp(image_path, num_points=8, radius=1):
     features = lbp_extractor.describe(image)
     return features
 
-
 def _process_images(src_img_dir, dst_img_dir, num_points=8, radius=1):
     """
     Processes images and masks within a directory, extracting LBP features
@@ -99,3 +98,87 @@ def _process_images(src_img_dir, dst_img_dir, num_points=8, radius=1):
         if os.path.isfile(image_path):
             image_lbp = _extract_lbp(image_path, num_points, radius)
             np.save(os.path.join(dst_img_dir, f"{base_filename}.npy"), image_lbp)
+
+def bf(src_dir, dst_dir, d=9, sigmaColor=75, sigmaSpace=75):
+    """
+    Recursively processes all images and masks within a directory structure,
+    performing Bilateal Filtering and saving them in a corresponding structure.
+
+    Args:
+        src_dir (str): Path to the source directory (raw/ in your case).
+        dst_dir (str): Path to the destination directory (bf/ in your case).
+        d (int, optional): Diameter of each pixel neighborhood.
+        sigmaColor (int, optional): Value of \sigma in the color space.
+            The greater the value, the colors farther to each other will start to get mixed.
+        sigmaSpace (int, optional): Value of \sigma in the coordinate space.
+            The greater its value, the more further pixels will mix together
+            , given that their colors lie within the sigmaColor range.        
+    """
+    for subdir in os.listdir(src_dir):
+        # Skip non-directory entries
+        if not os.path.isdir(os.path.join(src_dir, subdir)):
+            continue
+
+        # Create corresponding subdirectory in destination
+        dst_subdir = os.path.join(dst_dir, subdir)
+        # Create directory if it doesn't exist
+        os.makedirs(dst_subdir, exist_ok=True)
+        # Process images and masks within the subdirectory
+        _filter_images(os.path.join(src_dir, subdir, "images"),
+                        os.path.join(dst_subdir, "images"), d, sigmaColor, sigmaSpace)
+        # _filter_images(os.path.join(src_dir, subdir, "masks"),
+        #                 os.path.join(dst_subdir, "masks"), d, sigmaColor, sigmaSpace)
+
+def _filter_image(image_path, d=9, sigmaColor=25, sigmaSpace=25):
+    """
+    Performs bilateral filtering on an image and returns the filtered image.
+
+    Args:
+        image_path (str): Path to the image file.
+        d (int, optional): Diameter of each pixel neighborhood.
+        sigmaColor (int, optional): Value of \sigma in the color space.
+            The greater the value, the colors farther to each other will start to get mixed.
+        sigmaSpace (int, optional): Value of \sigma in the coordinate space.
+            The greater its value, the more further pixels will mix together
+            , given that their colors lie within the sigmaColor range.
+    Returns
+        cv2.image
+    """
+    # Load image (assuming grayscale)
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    filtered_image = cv2.bilateralFilter(image, d=d, sigmaColor=sigmaColor, sigmaSpace=sigmaSpace)
+    return filtered_image
+
+def _filter_images(src_img_dir, dst_img_dir, d=9, sigmaColor=75, sigmaSpace=75):
+    """
+    Processes images and masks within a directory, performing bilaterl filtering
+    and saving them with the same filename (excluding extension) in the destination directory.
+
+    Args:
+        src_img_dir (str): Path to the source directory containing images/masks.
+        dst_img_dir (str): Path to the destination directory for Bilaterl Filtering.
+        d (int, optional): Diameter of each pixel neighborhood.
+        sigmaColor (int, optional): Value of \sigma in the color space.
+            The greater the value, the colors farther to each other will start to get mixed.
+        sigmaSpace (int, optional): Value of \sigma in the coordinate space.
+            The greater its value, the more further pixels will mix together
+            , given that their colors lie within the sigmaColor range.
+
+    """
+    for filename in tqdm(os.listdir(src_img_dir), desc=f"Processing {src_img_dir}"):
+        # Skip non-image files
+        if not filename.lower().endswith((".png", ".jpg", ".jpeg")):
+            continue
+
+        # Get image and mask paths
+        image_path = os.path.join(src_img_dir, filename)
+        base_filename, _ = os.path.splitext(filename)
+        # dst_img_dir = os.path.abspath(dst_img_dir)
+        os.makedirs(dst_img_dir, exist_ok=True)
+
+        # Extract LBP for image
+        if os.path.isfile(image_path):
+            filtered_image = _filter_image(image_path, d, sigmaColor, sigmaSpace)
+            result = cv2.imwrite(os.path.join(dst_img_dir, f"{base_filename}.png"), filtered_image)
+            print(os.path.join(dst_img_dir, f"{base_filename}.png -> "), result, type(filtered_image))
