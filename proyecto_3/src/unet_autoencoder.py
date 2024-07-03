3ff8ef335c5aafe68cee408b092b43d190d9317d
Ariel Arévalo Alvarado <arielarevaloalvarado@gmail.com>
2024-07-02 18:53:02 -0600

    Proyecto 3: Agrega Autoencoder

--- proyecto_3/src/unet_autoencoder.py
+++ proyecto_3/src/unet_autoencoder.py
@@ -2,155 +2,121 @@ import torch
 import torch.nn as nn
 
 
-class UNetAutoencoder(nn.Module):
-    def __init__(self, in_channels=3, out_channels=3, latent_dim=256):
-        self.encoder = UNetEncoder(in_channels, latent_dim)
-        self.decoder = UNetDecoder(out_channels)
+def conv_block(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
 
-    def __init__(self, encoder, decoder):
-        super(UNetAutoencoder, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
 
-    def conv_block(in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+def latent_block(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(in_channels, out_channels),
+    )
 
-    def bottleneck_block(in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, in_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True)
-        )
 
-    def latent_block(in_channels, out_channels):
-        return nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten(),
-            nn.Linear(in_channels, out_channels)
-        )
+def max_pool():
+    return nn.MaxPool2d(kernel_size=2, stride=2)
 
-    def max_pool():
-        return nn.MaxPool2d(kernel_size=2, stride=2)
 
-    def up_conv(in_channels, out_channels):
-        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+def up_conv(in_channels, out_channels):
+    return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
 
-    def forward(self, x):
-        # Encoder
-        enc1 = self.encoder1(x)
-        enc1_pool = self.pool1(enc1)
 
-        enc2 = self.encoder2(enc1_pool)
-        enc2_pool = self.pool2(enc2)
-
-        enc3 = self.encoder3(enc2_pool)
-        enc3_pool = self.pool3(enc3)
-
-        enc4 = self.encoder4(enc3_pool)
-        enc4_pool = self.pool4(enc4)
-
-        # Bottleneck
-        bottleneck = self.bottleneck(enc4_pool)
-
-        # Latent vector
-        latent_vector = self.latent(bottleneck)
-
-        # Decoder
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.decoder4(dec4)
-
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
-
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
-
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
-
-        # Final layer
-        output = self.final_layer(dec1)
-
-        return output, latent_vector
+def conv_1x1(in_channels, out_channels):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
 
 class UNetEncoder(nn.Module):
-    def __init__(self, in_channels=3, latent_dim=256):
+    def __init__(self, in_channels=3, latent_dim=512):
         super(UNetEncoder, self).__init__()
-        # Encoder
-        self.encoder1 = UNetAutoencoder.conv_block(in_channels, 64)
-        self.pool1 = UNetAutoencoder.max_pool()
-        self.encoder2 = UNetAutoencoder.conv_block(64, 128)
-        self.pool2 = UNetAutoencoder.max_pool()
-        self.encoder3 = UNetAutoencoder.conv_block(128, 256)
-        self.pool3 = UNetAutoencoder.max_pool()
 
-        # Bottleneck
-        self.bottleneck = UNetAutoencoder.bottleneck_block(256, 512)
+        self.conv_1 = conv_block(in_channels, 64)
+        self.pool_1 = max_pool()
+        self.conv_2 = conv_block(64, 128)
+        self.pool_2 = max_pool()
+        self.conv_3 = conv_block(128, 256)
+        self.pool_3 = max_pool()
+        self.conv_4 = conv_block(256, 512)
+        self.pool_4 = max_pool()
+        self.conv_5 = conv_block(512, 1024)
 
-        # Latent vector layer
-        self.latent = UNetAutoencoder.latent_block(256, latent_dim)
+        self.latent = latent_block(1024 * 14 * 14, latent_dim)
 
     def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc1_pool = self.pool1(enc1)
+        enc_x1 = self.conv_1(x)
+        pool_x = self.pool_1(enc_x1)
 
-        enc2 = self.encoder2(enc1_pool)
-        enc2_pool = self.pool2(enc2)
+        enc_x2 = self.conv_2(pool_x)
+        pool_x = self.pool_2(enc_x2)
 
-        enc3 = self.encoder3(enc2_pool)
-        enc3_pool = self.pool3(enc3)
+        enc_x3 = self.conv_3(pool_x)
+        pool_x = self.pool_3(enc_x3)
 
-        bottleneck = self.bottleneck(enc3_pool)
+        enc_x4 = self.conv_4(pool_x)
+        pool_x = self.pool_4(enc_x4)
 
-        return bottleneck, [enc1, enc2, enc3]
+        bottleneck = self.conv_5(pool_x)
+
+        return bottleneck, [enc_x1, enc_x2, enc_x3, enc_x4]
 
     def extract_latent(self, x):
         bottleneck, _ = self.forward(x)
-        latent_vector = self.latent(bottleneck)
-        return latent_vector
+        latent_vectors = self.latent(bottleneck)
+        return latent_vectors
 
 
 class UNetDecoder(nn.Module):
     def __init__(self, out_channels=3):
         super(UNetDecoder, self).__init__()
-        self.upconv3 = self.up_conv(256, 256)
-        self.dec3 = self.upconv_block(256, 128)
-        self.upconv2 = self.up_conv(128, 128)
-        self.dec2 = self.upconv_block(128, 64)
-        self.upconv1 = self.up_conv(64, 64)
-        self.dec1 = self.upconv_block(64, out_channels)
 
-        self.final_layer = nn.Conv2d(64, out_channels, kernel_size=1)
+        self.up_conv_4 = up_conv(1024, 512)
+        self.conv_4 = conv_block(1024, 512)
+        self.up_conv_3 = up_conv(512, 256)
+        self.conv_3 = conv_block(512, 256)
+        self.up_conv_2 = up_conv(256, 128)
+        self.conv_2 = conv_block(256, 128)
+        self.up_conv_1 = up_conv(128, 64)
+        self.conv_1 = conv_block(128, 64)
 
-    def forward(self, bottleneck, enc_feats):
-        enc1, enc2, enc3 = enc_feats
+        self.final = conv_1x1(64, out_channels)
 
-        dec3 = self.upconv3(bottleneck)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
+    def forward(self, x, skips):
+        enc_x1, enc_x2, enc_x3, enc_x4 = skips
 
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
+        dec_x = self.up_conv_4(x)
+        dec_x = torch.cat((dec_x, enc_x4), dim=1)
+        dec_x = self.conv_4(dec_x)
 
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
+        dec_x = self.up_conv_3(dec_x)
+        dec_x = torch.cat((dec_x, enc_x3), dim=1)
+        dec_x = self.conv_3(dec_x)
 
-        output = self.final_layer(dec1)
-        return output
+        dec_x = self.up_conv_2(dec_x)
+        dec_x = torch.cat((dec_x, enc_x2), dim=1)
+        dec_x = self.conv_2(dec_x)
+
+        dec_x = self.up_conv_1(dec_x)
+        dec_x = torch.cat((dec_x, enc_x1), dim=1)
+        dec_x = self.conv_1(dec_x)
+
+        dec_x = self.final(dec_x)
+
+        return dec_x
+
+
+class UNetAutoencoder(nn.Module):
+    def __init__(self, encoder=UNetEncoder(in_channels=3, latent_dim=512), decoder=UNetDecoder(out_channels=3)):
+        super(UNetAutoencoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x):
+        bottleneck, skips = self.encoder(x)
+        dec = self.decoder(bottleneck, skips)
+        return dec
